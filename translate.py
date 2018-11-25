4fad80aa665cd790adf992cf649a66be6c554f18
Aryan Sharma <37960580+aryanharitsh123@users.noreply.github.com>
2018-11-25 10:24:32 +0530

    data.txt has all the information.

---
+++ translate.py
@@ -0,0 +1,32 @@
+import json as simplejson
+import requests
+import re
+
+num = 1
+r = requests.get("https://translate.sugarlabs.org/accounts/")
+str_to_search = r.text
+x = str_to_search.split("<table")[1].split("\n")[1:]
+x = "\n".join(x).split("</table>")[0]
+pattern=re.compile('<tr>(.*?)</tr>',re.DOTALL)
+data = re.findall(pattern, x)
+dat = {}
+with open('data.txt', 'a+', encoding='utf-8') as file:
+	for x in data[2:]:
+			#print(x)
+			x = x.split("\n")
+			dat['Username'] = x[2].strip()
+			dat['Firstname'] = re.compile('<td>(.*?)</td>').search(x[4]).group(1)
+			dat['Lastname'] = re.compile('<td>(.*?)</td>').search(x[4]).group(1).strip()
+			file.write(simplejson.dumps(dat) + "\n")
+
+
+
+
+
+
+#print(x)
+
+#print(str_to_search.split("<table")[1].split("/n")[1:].join("/n").split("</table>")[0])
+#pattern = re.compile(r'<a [^>]*href="([^"]+)')
+#print(ln(str_to_search.split("<table")))
+#print(x)
